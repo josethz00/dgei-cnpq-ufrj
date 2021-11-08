f568986cd3d942101ac2562cf5a5d72bed1c5676
josethz00 <jose.soares18@fatec.sp.gov.br>
2021-11-07 22:27:48 -0300

    refactor: secrets

--- server/app.js
+++ server/app.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 
 // substituir com a URL e dados do seu banco de dados
 // lembrar de mudar a senha e o nome do banco
-mongoose.connect('mongodb+srv://josethz00:zEzjC4YXZ37Em5T7@devcluster.u6g0p.mongodb.net/defesa_db?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://<seu-usuario-aqui>:<sua-senha-aqui>@devcluster.u6g0p.mongodb.net/<seu-banco-de-dados-aqui>?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(() => {
